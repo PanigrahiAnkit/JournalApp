3d74ea940d9db00224ca333d9b3593c4bc8b828c
Ankit Panigrahi <panigrahi0702@gmail.com>
2025-08-12 11:08:36 +0530

    feat(UserRepository.java): created a method findByUserName

--- src/main/java/com/myproject/journalApp/repository/UserRepository.java
+++ src/main/java/com/myproject/journalApp/repository/UserRepository.java
@@ -6,8 +6,9 @@ import org.bson.types.ObjectId;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
 public interface UserRepository extends MongoRepository<User, ObjectId> {
-
+    User findByUserName(String userName);
 }
+
 
 
 //controller ---> service ---> repository
