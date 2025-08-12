eac655b6a6cdf25e0ca554802b83e138c44c529f
Ankit Panigrahi <panigrahi0702@gmail.com>
2025-08-12 15:04:52 +0530

    feat(JournalApplication): added @Transactional annotatio to handle atomicity and updated the mongo uri application.properties

--- src/main/java/com/myproject/journalApp/JournalApplication.java
+++ src/main/java/com/myproject/journalApp/JournalApplication.java
@@ -2,12 +2,28 @@ package com.myproject.journalApp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.MongoDatabaseFactory;
+import org.springframework.data.mongodb.MongoTransactionManager;
+import org.springframework.transaction.PlatformTransactionManager;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import java.beans.JavaBean;
 
 @SpringBootApplication
+@EnableTransactionManagement
 public class JournalApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(JournalApplication.class, args);
 	}
 
+	@Bean
+	public PlatformTransactionManager add(MongoDatabaseFactory dbFactory) { // To make connections with the db
+		return new MongoTransactionManager(dbFactory);
+	}
+
 }
+
+// PlatformTransactionManager - Interface
+// MongoTransactionManager - Implements the above
