0414c24fdd05d651a82b9aa23ff1e293f8c28952
Ankit Panigrahi <panigrahi0702@gmail.com>
2025-08-14 15:11:34 +0530

    refactor(controller): POST /journal now uses authenticated user (removed userName path param)

--- src/main/java/com/myproject/journalApp/controller/JournalEntryController.java
+++ src/main/java/com/myproject/journalApp/controller/JournalEntryController.java
@@ -39,11 +39,11 @@ public class JournalEntryController {
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    @PostMapping("{userName}")
-    public ResponseEntity<JournalEntry> createEntry(@RequestBody JournalEntry myEntry, @PathVariable String userName) { //
-        // localhost:8080/journal
-        // GET
+    @PostMapping
+    public ResponseEntity<JournalEntry> createEntry(@RequestBody JournalEntry myEntry) { //
         try {
+            Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+            String userName = authentication.getName();
             journalEntryService.saveEntry(myEntry, userName);
             return new ResponseEntity<>(myEntry, HttpStatus.CREATED);
         } catch(Exception e) {
