a9436fa97eaa936b83d88f28b0997793a8e6bcf1
Ankit Panigrahi <panigrahi0702@gmail.com>
2025-08-20 21:14:29 +0530

    feat: added api keys

---
+++ src/main/java/com/myproject/journalApp/service/QuoteService.java
@@ -0,0 +1,6 @@
+package com.myproject.journalApp.service;
+
+public class QuoteService {
+
+    private static final String apiKey = "TZtQ3OBbGVTRdjlxrop/Nw==e2h22ZbnEQJ6AG3Q";
+}
---
+++ src/main/java/com/myproject/journalApp/service/WeatherService.java
@@ -0,0 +1,7 @@
+package com.myproject.journalApp.service;
+
+public class WeatherService {
+
+    private static final String apiKey = "ad7e47c5c483879c017c178f2722e8c9";
+
+}
