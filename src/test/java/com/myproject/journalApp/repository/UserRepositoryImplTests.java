65c5910c80b053eed96fe910288c3ac3637a29f2
Ankit Panigrahi <panigrahi0702@gmail.com>
2025-09-02 14:43:55 +0530

    feat: created a test class for Redis Tests in service package

--- src/test/java/com/myproject/journalApp/repository/UserRepositoryImplTests.java
+++ src/test/java/com/myproject/journalApp/repository/UserRepositoryImplTests.java
@@ -1,19 +1,12 @@
 package com.myproject.journalApp.repository;
 
-import com.myproject.journalApp.entity.User;
-import com.myproject.journalApp.service.UserArgumentsProvider;
-import lombok.RequiredArgsConstructor;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.params.ParameterizedTest;
-import org.junit.jupiter.params.provider.ArgumentsSource;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.Mockito.*;
 
 @SpringBootTest
 public class UserRepositoryImplTests {
