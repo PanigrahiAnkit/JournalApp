1374b4a52ff01a321bfd310aed49562ceb56bc7b
Ankit Panigrahi <panigrahi0702@gmail.com>
2025-09-02 14:55:24 +0530

    feat: created Redis config

---
+++ src/main/java/com/myproject/journalApp/config/RedisConfig.java
@@ -0,0 +1,4 @@
+package com.myproject.journalApp.config;
+
+public class RedisConfig {
+}
--- src/test/java/com/myproject/journalApp/service/RedisTests.java
+++ src/test/java/com/myproject/journalApp/service/RedisTests.java
@@ -1,4 +1,20 @@
 package com.myproject.journalApp.service;
 
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.data.redis.core.RedisTemplate;
+
+@SpringBootTest
 public class RedisTests {
+
+    @Autowired
+    private RedisTemplate redisTemplate;
+
+    @Test
+    void testSendMail() {
+//        redisTemplate.opsForValue().set("email","noveh41221@cavoyar.com");
+        Object email = redisTemplate.opsForValue().get("email");
+        int a = 1;
+    }
 }
