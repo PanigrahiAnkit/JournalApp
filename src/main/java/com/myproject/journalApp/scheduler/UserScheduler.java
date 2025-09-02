2c4903935b4ee2ad53077eea1ea22c6513a6f477
Ankit Panigrahi <panigrahi0702@gmail.com>
2025-09-02 11:49:04 +0530

    refactor: now fetching List of Sentiments instead of String content

--- src/main/java/com/myproject/journalApp/scheduler/UserScheduler.java
+++ src/main/java/com/myproject/journalApp/scheduler/UserScheduler.java
@@ -3,6 +3,7 @@ package com.myproject.journalApp.scheduler;
 import com.myproject.journalApp.cache.AppCache;
 import com.myproject.journalApp.entity.JournalEntry;
 import com.myproject.journalApp.entity.User;
+import com.myproject.journalApp.enums.Sentiment;
 import com.myproject.journalApp.repository.UserRepositoryImpl;
 import com.myproject.journalApp.service.EmailService;
 import com.myproject.journalApp.service.SentimentAnalysisService;
@@ -23,12 +24,14 @@ public class UserScheduler {
     private final UserRepositoryImpl userRepository;
     private final AppCache appCache;
 
-    @Scheduled(cron = "0 0 9 * * SUN")
+//    @Scheduled(cron = "0 0 9 * * SUN")
     public void fetchUsersAndSendMail() {
         List<User> users = userRepository.getUserForSA();
-        for(User user: users) {
+        for(User user : users) {
             List<JournalEntry> journalEntries = user.getJournalEntries();
-            List<String> filteredEntries = journalEntries.stream().filter(x -> x.getDate().isAfter(LocalDateTime.now().minus(7, ChronoUnit.DAYS))).map(x -> x.getContent()).collect(Collectors.toList());
+            List<Sentiment> filteredEntries =
+                    journalEntries.stream().filter(x -> x.getDate().isAfter(LocalDateTime.now().minus(7,
+                            ChronoUnit.DAYS))).map(x -> x.getSentiment()).collect(Collectors.toList());
             String entry = String.join(" ", filteredEntries);
             String sentiment = sentimentAnalysisService.getSentiment(entry);
             emailService.sendEmail(user.getEmail(), "Sentiment for last 7 days", sentiment);
