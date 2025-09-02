fd0f5382a764c5c7f013890be0d5b6d6ee32fb5b
Ankit Panigrahi <panigrahi0702@gmail.com>
2025-09-02 15:25:56 +0530

    feat: created a method to get the value stored in redis

--- src/main/java/com/myproject/journalApp/service/RedisService.java
+++ src/main/java/com/myproject/journalApp/service/RedisService.java
@@ -3,18 +3,25 @@ package com.myproject.journalApp.service;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.myproject.journalApp.api.response.WeatherResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class RedisService {
 
     private final RedisTemplate redisTemplate;
 
-    public void get(String key, Class<WeatherResponse> weatherResponseClass) {
-        Object o = redisTemplate.opsForValue().get(key);
-        ObjectMapper mapper = new ObjectMapper();
-        return mapper.readValues(o.toString() )
+    public <T> T get(String key, Class<T> entityClass) {
+        try{
+            Object o = redisTemplate.opsForValue().get(key);
+            ObjectMapper mapper = new ObjectMapper();
+            return mapper.readValue(o.toString(), entityClass);
+        } catch(Exception e) {
+            log.error("Exception:  ", e);
+            return null;
+        }
     }
 }
