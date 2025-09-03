5e2719a8542e87fb902cc980ddc14728bb414702
Ankit Panigrahi <panigrahi0702@gmail.com>
2025-09-03 14:19:00 +0530

    feat(model): added email and sentiment fields to be used by kafka

--- src/main/java/com/myproject/journalApp/model/SentimentData.java
+++ src/main/java/com/myproject/journalApp/model/SentimentData.java
@@ -1,4 +1,15 @@
 package com.myproject.journalApp.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class SentimentData {
+    private String email;
+    private String sentiment;
 }
