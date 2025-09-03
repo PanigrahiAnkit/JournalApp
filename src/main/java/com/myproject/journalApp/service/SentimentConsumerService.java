da9603428243a7d0bddb87eafc4e7e717de3f428
Ankit Panigrahi <panigrahi0702@gmail.com>
2025-09-03 14:24:30 +0530

    feat(service): added Consumer Logic to retrieve sentiment data as email

--- src/main/java/com/myproject/journalApp/service/SentimentConsumerService.java
+++ src/main/java/com/myproject/journalApp/service/SentimentConsumerService.java
@@ -1,7 +1,22 @@
 package com.myproject.journalApp.service;
 
+import com.myproject.journalApp.model.SentimentData;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Service;
 
 @Service
+@RequiredArgsConstructor
 public class SentimentConsumerService {
+
+    private final EmailService emailService;
+
+    @KafkaListener(topics = "weekly-sentiments", groupId = "weekly-sentiment-group")
+    public void consume(SentimentData sentimentData) {
+        sendEmail(sentimentData);
+    }
+
+    private void sendEmail(SentimentData sentimentData) {
+        emailService.sendEmail(sentimentData.getEmail(), "Sentiment for previous week", sentimentData.getSentiment());
+    }
 }
