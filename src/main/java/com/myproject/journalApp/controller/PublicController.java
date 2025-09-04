c54f886be9fb08fb3a88ed8cbcba7bf7bf9deec6
Ankit Panigrahi <panigrahi0702@gmail.com>
2025-09-04 14:09:13 +0530

    feat: added methods in JwtUtils

--- src/main/java/com/myproject/journalApp/controller/PublicController.java
+++ src/main/java/com/myproject/journalApp/controller/PublicController.java
@@ -1,16 +1,27 @@
 package com.myproject.journalApp.controller;
 
 import com.myproject.journalApp.entity.User;
+import com.myproject.journalApp.service.UserDetailsServiceImpl;
 import com.myproject.journalApp.service.UserService;
+import com.myproject.journalApp.utils.JwtUtil;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@RequiredArgsConstructor
 @RequestMapping("/public")
 public class PublicController {
 
+
+    private final UserService userService;
+    private final UserDetailsServiceImpl userDetailsService;
     @Autowired
-    private UserService userService;
+    private AuthenticationManager authenticationManager;
+    private final JwtUtil jwtUtil;
 
     @GetMapping("/health-check")
     public String healthCheck() {
@@ -24,6 +35,14 @@ public class PublicController {
 
     @PostMapping("/login")
     public void login(@RequestBody User user) {
-        userService.saveNewUser(user);
+        try {
+            authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(user.getUserName(),
+                    user.getPassword()));
+            UserDetails userDetails = userDetailsService.loadUserByUsername(user.getUserName());
+            jwtUtil.
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
     }
 }
